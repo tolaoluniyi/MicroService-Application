78e2039415feea79938a8b27de0c4ab39d66e3d9
Temi <170970335+tolaoluniyi@users.noreply.github.com>
2024-07-06 10:33:47 -0400

    jenkins.tf

--- Jenkins/Jenkins.tf
+++ Jenkins/Jenkins.tf
@@ -94,7 +94,7 @@ data "aws_ami" "ubuntu" {
 # launch the ec2 instance
 resource "aws_instance" "ec2_instance" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.small"
+  instance_type          = "t3.2xlarge"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   key_name               = "devopskeypair"
